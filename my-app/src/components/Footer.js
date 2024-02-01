39ce611d8777f0eb12242e231bb750a6595faf27
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-02-01 18:47:20 +0530

    Navbar,Footer Components

---
+++ my-app/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>&copy; 2024 Coupons Code X-change Plateform</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <nav className='navbar'>
+        <div className='leftNav'>
+            Coupons Code Plateform
+        </div>
+        <div className='rightLogo'>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Products</li>
+        </ul>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
